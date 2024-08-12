83ed953c84c0143638e80deda22b7eb901a114ba
Avi Dargan <36219266+avidargan@users.noreply.github.com>
2024-08-12 16:09:04 +1000

    Changed metadata and added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,40 @@ const roboto = Roboto({ subsets: ["latin"], weight: ["100", "300", "400", "500",
 
 export const metadata: Metadata = {
   title: "Avi Dargan - Software Engineer",
-  description: "A website by Avi Dargan",
+  description: "Hello, my name is Avi Dargan, and I'm a Software Engineer. Check out my featured projects to see what I'm working on!",
+  icons: {
+    icon: [
+      {
+        rel: "icon",
+        type: "image/ico",
+        url: "/favicon/favicon.ico",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        url: "/favicon/favicon-16x16.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        url: "/favicon/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "192x192",
+        url: "/favicon/android-chrome-192x192.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "512x512",
+        url: "/favicon/android-chrome-512x512.png",
+      },
+    ],
+  }
 };
 
 export default function RootLayout({
